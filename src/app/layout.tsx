32e53c6be73c52eadc75644483a9ff2db8296268
Sploob <crayiphone@gmail.com>
2025-04-06 13:52:07 -0700

    working auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import "~/styles/globals.css";
-
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
+
 import Navbar from "~/components/Navbar";
 
 export const metadata: Metadata = {
@@ -19,11 +22,13 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
+    <ClerkProvider>
     <html lang="en" className={`${geist.variable}`}>
       <body>
         {children}
         <Navbar />
       </body>
     </html>
+    </ClerkProvider>
   );
 }
