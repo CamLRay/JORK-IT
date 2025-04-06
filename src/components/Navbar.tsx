0c2fb889c0d7b13baf95ab37e16bb5b4fb2738e9
Sploob <crayiphone@gmail.com>
2025-04-06 12:43:38 -0700

    WIP coolify on VPS

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,14 +5,14 @@ export default function Navbar() {
   const menu =[
     { name: "dashboard", path: '/', icon: ["material-symbols:dashboard-outline", "material-symbols:dashboard" ] }, 
     { name: "history", path: '/history', icon: ["material-symbols-light:book-outline", "material-symbols-light:book" ] }, 
-    { name: "", path: '/', icon: ["f7:plus-app-fill", "f7:plus-app-fill"] }, 
+    { name: "", path: '', icon: ["f7:plus-app-fill", "f7:plus-app-fill"] }, 
     { name: "exercises", path: '/exercises', icon: ["healthicons:exercise-outline", "healthicons:exercise"] },
     { name: "more", path: '/more', icon: ["fluent:more-circle-32-regular", "fluent:more-circle-32-filled"] } 
   ]
   return(
     <nav className="fixed bottom-0 left-0 bg-zinc-900 text-stone-200 min-w-full grid grid-cols-5 gap-2 place-items-center border-t-1 border-t-zinc-400">
       {menu.map((option, index) => (
-        <Link key={index} href={"/" + option.name} className="capitalize grid grid-rows-1 place-items-center py-2 text-xs" ><Icon icon={option.icon[1]!} className="text-3xl text-white"/>{option.name}</Link>
+        <Link key={index} href={option.path} className="capitalize grid grid-rows-1 place-items-center py-2 text-xs" ><Icon icon={option.icon[1]!} className="text-3xl text-white"/>{option.name}</Link>
       ))}
     </nav>
   );
--- src/env.js
+++ src/env.js
@@ -8,6 +8,20 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
+    POSTGRES_USER: z.string(),
+    POSTGRES_PASSWORD: z.string(),
+    POSTGRES_DB: z.string(),
+    // POSTGRES_URL: z.string().url(),
+    // POSTGRES_PRISMA_URL: z.string().url(),
+    // SUPABASE_URL: z.string().url(),
+    // POSTGRES_URL_NON_POOLING: z.string().url(),
+    // SUPABASE_JWT_SECRET: z.string(),
+    // POSTGRES_USER: z.string(),
+    // POSTGRES_PASSWORD: z.string(),
+    // POSTGRES_DATABASE: z.string(),
+    // SUPABASE_SERVICE_ROLE_KEY: z.string(),
+    // POSTGRES_HOST: z.string(),
+    // SUPABASE_ANON_KEY: z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -20,6 +34,8 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    // NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+    // NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
   },
 
   /**
@@ -28,8 +44,24 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    POSTGRES_DB: process.env.POSTGRES_DB,
+    // POSTGRES_URL: process.env.POSTGRES_URL,
+    // POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    // SUPABASE_URL: process.env.SUPABASE_URL,
+    // POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+    // SUPABASE_JWT_SECRET: process.env.SUPABASE_JWT_SECRET,
+    // POSTGRES_USER: process.env.POSTGRES_USER,
+    // POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    // POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
+    // SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    // POSTGRES_HOST: process.env.POSTGRES_HOST,
+    // SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    // NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    // NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
