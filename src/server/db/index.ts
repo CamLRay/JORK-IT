54a08bf727ff87d35f1a3ca9213d2075c4453d29
Sploob <crayiphone@gmail.com>
2025-04-05 23:55:23 -0700

    mocked up nav bar

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -8,11 +8,6 @@ import * as schema from "./schema";
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
-};
-
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
-
-export const db = drizzle(conn, { schema });
+if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const client = postgres(env.DATABASE_URL);
+export const db = drizzle(client);
